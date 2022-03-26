180e25d0ff3657af40920dfbb9f03b9fc56d90ba
Nizlatakar <nizlatakar@gmail.com>
2022-03-26 15:18:09 +0200

    up

--- тест/test.js
+++ тест/test.js
@@ -1,11 +1,53 @@
-function sum(arr1, rotations) {
-    
-    for (let index = 0; index < rotations.length; index++) {
-    let newArr=arr1.shift()
-      arr1.push(newArr)
+function solve(input) {
+  let text = input.shift();
+
+  for (let line of input) {
+    line = line.split("|");
+    if (line[0] === "Move") {
+      let first = text.slice(0, Number(line[1]))
+      text = first;
+
     }
+    
 
-    console.log(arr1.join(' '));
+    if (line[0] === "ChangeAll") {
+
+      for (let letter of text) {
+        let another = letter.replace(line[1], line[2])
+        text = text + another.concat()
+
+      }
+      
+
+    }
+    if (line[0] === "Insert") {
+      let index = Number(line[1])
+      let value = line[2]
+      let cutted = text.slice(index)
+      text = cutted + value
+      console.log(text);
+
+    }
+  
+     if (line[0] === "Decode") {
+      console.log(`The decrypted message is: ${text}`);
+    }
+  }
+
 }
+solve([
+  'zzHe',
+  'ChangeAll|z|l',
+  'Insert|2|o',
+  'Move|3',
+  'Decode',
+])
+solve([
+  'owyouh',
+  'Move|2',
+  'Move|3',
+  'Insert|3|are',
+  'Insert|9|?',
+  'Decode',
+])
 
-sum([51, 47, 32, 61, 21], 2);
