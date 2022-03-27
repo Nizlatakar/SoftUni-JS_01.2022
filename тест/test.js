d6df4988bb81049b28e99f670bc83b3b4f3f3980
Nizlatakar <nizlatakar@gmail.com>
2022-03-27 20:51:22 +0300

    up

--- тест/test.js
+++ тест/test.js
@@ -1,39 +1,40 @@
-function solve(input) {
-  let text = input.shift();
+function worldTour(input) {
+  //take encoded message
+  let stops = input.shift();
+  for (let element of input) {
+    //console.log(element); //Add Stop:7:Rome
+    let tokens = element.split(":");
+    //console.log(tokens); //['Add Stop', '7', 'Rome']
+    let command = tokens[0];
+    //console.log(command); //Add Stop
+    if (command === "Add Stop") {
+      let index = +tokens[1];
+      let str = tokens[2];
 
-  for (let line of input) {
-    line = line.split("|");
-    if (line[0] === "Move") {
-      let first = text.slice(0, Number(line[1]))
-      text = first;
-
-    }
-    
-
-    if (line[0] === "ChangeAll") {
-
-      for (let letter of text) {
-        let another = letter.replace(line[1], line[2])
-        text = text + another.concat()
-
+      let firstHalf = stops.substring(0, index);
+      let secondHalf = stops.substring(index);
+      stops = firstHalf + str + secondHalf;
+      console.log(stops);
+    } else if (command === "Remove Stop") {
+      let startIndex = +tokens[1];
+      let endIndex = +tokens[2];
+      //console.log(startIndex, endIndex); //11 16
+      removed = stops.slice(startIndex, endIndex + 1);
+      stops = stops.replace(removed, "");
+      console.log(stops);
+    } else if (command === "Switch") {
+      let oldString = tokens[1];
+      let newString = tokens[2];
+      //console.log(oldString, newString); //Hawai Bulgaria
+      if (stops.includes(oldString)) {
+        stops = stops.replace(oldString, newString);
       }
-      
-
-    }
-    if (line[0] === "Insert") {
-      let index = Number(line[1])
-      let value = line[2]
-      let cutted = text.slice(index)
-      text = cutted + value
-      console.log(text);
-
-    }
-  
-     if (line[0] === "Decode") {
-      console.log(`The decrypted message is: ${text}`);
+      console.log(stops);
+    } else if (command === "Travel") {
+      console.log(`Ready for world tour! Planned stops: ${stops}`);
+      break;
     }
   }
-
 }
 solve([
   'zzHe',
