b33b1fa89cdfc854cb35fc2a22c4d22471965850
Nizlatakar <nizlatakar@gmail.com>
2022-03-30 15:48:29 +0300

    up

--- тест/test 01.js
+++ тест/test 01.js
@@ -1,12 +1,16 @@
-function solve(input,searched) {
-    let conter =0
-while(input.includes(searched)){
-input=input.replace(searched,"@".repeat(searched.length))
- 
+function solve(searched,text) {
+ let [fword,sword]=  searched.split(', ')
+ text.split(" ")
+for (const word of text) {
+    while(word.includes(searched)){
+    input=input.replace(searched,"@".repeat(searched.length))
+     
+    }
 }
 console.log(input); 
 }
 
 
 
-solve("this is simpl text to show is this  for is this","is")
+solve('great, learning',
+'softuni is ***** place for ******** new programming languages')
