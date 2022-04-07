ec9013d3bb7a41c0165e9e344f1f5dc1c9ae8a08
Nizlatakar <nizlatakar@gmail.com>
2022-04-07 19:03:17 +0300

    fdf

--- 06 Objects and classes/06 Exercise/01 Employees.js
+++ 06 Objects and classes/06 Exercise/01 Employees.js
@@ -1,5 +1,15 @@
 function employees(input) {
  input.forEach(element=>console.log(`Name: ${element} -- Personal Number: ${element.length}`)); 
+//  function solve(input) {
+//     for (const singleName of input) {
+//         let person = {
+//             name: singleName,
+//             number: singleName.length
+//         }
+//         console.log(`Name: ${person.name} -- Personal Number: ${person.number}`);
+//     }
+// }
+
 
 }
 
