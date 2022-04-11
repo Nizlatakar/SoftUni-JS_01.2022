4607f16f7dee2e91e7aec8333a504c50ca20dbc7
Nizlatakar <nizlatakar@gmail.com>
2022-04-11 09:15:58 +0300

    up

--- 06 Objects and classes/06 Exercise/05 Inventory.js
+++ 06 Objects and classes/06 Exercise/05 Inventory.js
@@ -1,14 +1,14 @@
 function solve(arr) {
 
-    let heros = []
+    let heroes = []
 
     arr.map(x => x.split(' / '))
 
-        .map(x => heros.push({ name: x[0], level: +x[1], items: x[2].split(', ').join(', ') }))
+        .map(x => heroes.push({ name: x[0], level: +x[1], items: x[2].split(', ').join(', ') }))
 
 
 
-    heros.sort((a, b) => a.level - b.level)
+        heroes.sort((a, b) => a.level - b.level)
 
         .map(x => console.log(`Hero: ${x.name}\nlevel => ${x.level}\nitems => ${x.items}`))
 
--- тест/object test.js
+++ тест/object test.js
@@ -1,40 +1,34 @@
-function solve(arr1, arr2) {
-    let available = {};
-    for (let index = 0; index < arr1.length; index += 2) {
-        let product = arr1[index]
-        available[product] = Number(arr1[index + 1])
-
+function solve(input) {
+for (const line of input) {
+    let[_,student,grade,score] = line.split(': ')
+    student = student.split(', ').shift();
+    grade= Number(grade.split(', ').shift())
+    score= Number(score)
+    let register={
+        student,
+        grade,
+        score
+    }
+    if (register[score]<3){
+        register=register[student].remove()
     }
 
-    for (let i = 0; i < arr2.length; i += 2) {
-        let needed = arr2[i]
-
-        if (!available.hasOwnProperty(needed)) {
-            available[needed] = 0;
-
-        }
-        available[needed] += Number(arr2[i + 1]);
-    }
-    for (const needed in available) {
-        console.log(`${needed} -> ${available[needed]}`)
-
-    }
-    // sort by value
-    let entries = Object.entries(available).sort((a,b) =>{
-        return a[1]-b[1]
-    })
-    entries.forEach(el=>{
-        console.log(el);
-    })
-    
+    console.log();
+    console.log();
+    console.log(register);
 }
-
-
-
-
+}
 solve([
-    'Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'
-],
-    [
-        'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
+    "Student name: Mark, Grade: 8, Graduated with an average score: 4.75",
+        "Student name: Ethan, Grade: 9, Graduated with an average score: 5.66",
+        "Student name: George, Grade: 8, Graduated with an average score: 2.83",
+        "Student name: Steven, Grade: 10, Graduated with an average score: 4.20",
+        "Student name: Joey, Grade: 9, Graduated with an average score: 4.90",
+        "Student name: Angus, Grade: 11, Graduated with an average score: 2.90",
+        "Student name: Bob, Grade: 11, Graduated with an average score: 5.15",
+        "Student name: Daryl, Grade: 8, Graduated with an average score: 5.95",
+        "Student name: Bill, Grade: 9, Graduated with an average score: 6.00",
+        "Student name: Philip, Grade: 10, Graduated with an average score: 5.05",
+        "Student name: Peter, Grade: 11, Graduated with an average score: 4.88",
+        "Student name: Gavin, Grade: 10, Graduated with an average score: 4.00"
     ])
